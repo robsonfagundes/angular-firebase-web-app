50e5851399413c2b425f81c135a62ccd20766d32
robsonfagundes <robsonfagundes@gmail.com>
2017-03-20 23:16:53 -0300

    Add Firebase dependencies

--- app/app.js
+++ app/app.js
@@ -1,8 +1,12 @@
 'use strict';
 
 angular.module('angular-firebase-web-app', [
-	'ngRoute'
+	'ngRoute',
+	'angular-firebase-web-app.home'
 ]).
 config(['$routeProvider', function($routeProvider) {
 	// Routes will be here
+	$routeProvider.otherwise({
+		redirectTo: '/home'
+	});
 }]);
