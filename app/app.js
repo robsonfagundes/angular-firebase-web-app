c199dc33ca51d94177901c594ac227961d0ddea6
robsonfagundes <robsonfagundes@gmail.com>
2017-03-21 00:02:27 -0300

    Add login

--- app/app.js
+++ app/app.js
@@ -3,10 +3,13 @@
 angular.module('angular-firebase-web-app', [
 	'ngRoute',
 	'angular-firebase-web-app.home'
+
 ]).
 config(['$routeProvider', function($routeProvider) {
 	// Routes will be here
+	
 	$routeProvider.otherwise({
 		redirectTo: '/home'
 	});
+
 }]);
--- app/home/home.js
+++ app/home/home.js
@@ -1,16 +1,42 @@
 'use strict';
- 
-angular.module('angular-firebase-web-app.home', ['ngRoute'])
- 
+
+angular.module('angular-firebase-web-app.home', ['ngRoute', 'firebase', 'firebaseSimpleLogin'])
+
 // Declared route 
 .config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/home', {
-        templateUrl: 'home/home.html',
-        controller: 'HomeCtrl'
-    });
+	$routeProvider.when('/home', {
+		templateUrl: 'home/home.html',
+		controller: 'HomeCtrl'
+	});
 }])
- 
+
 // Home controller
-.controller('HomeCtrl', [function() {
- 
-}]);
+.controller('HomeCtrl', [
+	'$scope',
+	'$firebase',
+	'$firebaseSimpleLogin',
+
+	function($scope) {
+
+		var firebaseObj = new Firebase("https://angular-firebase-45775.firebaseio.com");	
+		var loginObj = $firebaseSimpleLogin(firebaseObj);
+
+		$scope.SignIn = function(event) {
+			event.preventDefault(); // To prevent form refresh
+			var username = $scope.user.email;
+			var password = $scope.user.password;
+
+			loginObj.$login('password', {
+					email: username,
+					password: password
+				})
+				.then(function(user) {
+					// Success callback
+					console.log('Authentication successful');
+				}, function(error) {
+					// Failure callback
+					console.log('Authentication failure');
+				});
+		}
+	}
+]);
